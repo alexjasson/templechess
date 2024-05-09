9c0cc2ec8af41e0bc023fad9958edf191f57344c
Alex Jasson <z5390820@ad.unsw.edu.au>
2024-05-09 11:23:14 +1000

    remove gunk

--- src/LookupTable.c
+++ src/LookupTable.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
+#include <immintrin.h>
 
 #include "BitBoard.h"
 #include "LookupTable.h"
 
 #define IS_DIAGONAL(d) (d % 2 == 1)
-#define POWERSET_SIZE(n) (1 << n)
 #define BISHOP_ATTACKS_POWERSET 512
 #define ROOK_ATTACKS_POWERSET 4096
-#define MAGIC_NUMBERS "data/magicNumbers.out"
 
 typedef enum {
   North, Northeast, East, Southeast, South, Southwest, West, Northwest
 } Direction;
-
-typedef struct {
-  BitBoard bits;
-  int bitShift;
-  uint64_t magicNumber;
-} Magic;
 
 struct lookupTable {
   BitBoard knightAttacks[BOARD_SIZE];
   BitBoard kingAttacks[BOARD_SIZE];
   BitBoard bishopAttacks[BOARD_SIZE][BISHOP_ATTACKS_POWERSET];
   BitBoard rookAttacks[BOARD_SIZE][ROOK_ATTACKS_POWERSET];
-  Magic bishopMagics[BOARD_SIZE]; // Used for bishop attacks
-  Magic rookMagics[BOARD_SIZE]; // Used for rook attacks
+  BitBoard bishopBits[BOARD_SIZE];
+  BitBoard rookBits[BOARD_SIZE];
 
   BitBoard squaresBetween[BOARD_SIZE][BOARD_SIZE]; // Squares Between exclusive
   BitBoard lineOfSight[BOARD_SIZE][BOARD_SIZE]; // All squares of a rank/file/diagonal/antidiagonal
@@ -36,16 +27,13 @@ struct lookupTable {
 
 static BitBoard getMove(Square s, Type t, Direction d, int steps);
 static BitBoard getAttacks(Square s, Type t, BitBoard occupancies);
-
 static BitBoard getRelevantBits(Square s, Type t);
 static BitBoard getBitsSubset(int index, BitBoard bits);
-static Magic getMagic(Square s, Type t, FILE *fp);
-static int magicHash(Magic m, BitBoard occupancies);
-static uint64_t getRandomU64();
-static uint32_t xorshift();
+static int hash(BitBoard occupancies, BitBoard relevantBits);
 
 static BitBoard getSquaresBetween(LookupTable l, Square s1, Square s2);
 static BitBoard getLineOfSight(LookupTable l, Square s1, Square s2);
+
 static void initializeLookupTable(LookupTable l);
 
 LookupTable LookupTableNew(void) {
@@ -61,38 +49,34 @@ LookupTable LookupTableNew(void) {
 }
 
 void initializeLookupTable(LookupTable l) {
-  FILE *fp = fopen(MAGIC_NUMBERS, "a+");
-  if (fp == NULL) {
-    fprintf(stderr, "Failed to open '%s' for reading/writing\n", MAGIC_NUMBERS);
-    exit(EXIT_FAILURE);
-  }
-
-  // Minor/major piece moves
-  for (Square s = a8; s <= h1; s++) {
-    l->bishopMagics[s] = getMagic(s, Bishop, fp);
-    l->rookMagics[s] = getMagic(s, Rook, fp);
-    l->knightAttacks[s] = getAttacks(s, Knight, EMPTY_BOARD);
-    l->kingAttacks[s] = getAttacks(s, King, EMPTY_BOARD);
+  for (Square s1 = a8; s1 <= h1; s1++) {
+    l->knightAttacks[s1] = getAttacks(s1, Knight, EMPTY_BOARD);
+    l->kingAttacks[s1] = getAttacks(s1, King, EMPTY_BOARD);
+    l->bishopBits[s1] = getRelevantBits(s1, Bishop);
+    l->rookBits[s1] = getRelevantBits(s1, Rook);
     for (int i = 0; i < BISHOP_ATTACKS_POWERSET; i++) {
-      BitBoard occupancies = getBitsSubset(i, l->bishopMagics[s].bits);
-      int index = magicHash(l->bishopMagics[s], occupancies);
-      l->bishopAttacks[s][index] = getAttacks(s, Bishop, occupancies);
+      BitBoard occupancies = getBitsSubset(i, l->bishopBits[s1]);
+      int index = hash(occupancies, l->bishopBits[s1]);
+      l->bishopAttacks[s1][index] = getAttacks(s1, Bishop, occupancies);
     }
     for (int i = 0; i < ROOK_ATTACKS_POWERSET; i++) {
-      BitBoard occupancies = getBitsSubset(i, l->rookMagics[s].bits);
-      int index = magicHash(l->rookMagics[s], occupancies);
-      l->rookAttacks[s][index] = getAttacks(s, Rook, occupancies);
+      BitBoard occupancies = getBitsSubset(i, l->rookBits[s1]);
+      int index = hash(occupancies, l->rookBits[s1]);
+      l->rookAttacks[s1][index] = getAttacks(s1, Rook, occupancies);
     }
   }
-  fclose(fp);
 
-  // Helper tables
   for (Square s1 = a8; s1 <= h1; s1++) {
     for (Square s2 = a8; s2 <= h1; s2++) {
       l->squaresBetween[s1][s2] = getSquaresBetween(l, s1, s2);
       l->lineOfSight[s1][s2] = getLineOfSight(l, s1, s2);
     }
   }
+}
+
+// We use the pext instruction as a perfect hash function
+int hash(BitBoard occupancies, BitBoard relevantBits) {
+  return _pext_u64(occupancies, relevantBits);
 }
 
 void LookupTableFree(LookupTable l) {
@@ -103,10 +87,10 @@ BitBoard LookupTableAttacks(LookupTable l, Square s, Type t, BitBoard occupancie
   switch (t) {
     case Knight: return l->knightAttacks[s];
     case King: return l->kingAttacks[s];
-    case Bishop: return l->bishopAttacks[s][magicHash(l->bishopMagics[s], occupancies)];
-    case Rook: return l->rookAttacks[s][magicHash(l->rookMagics[s], occupancies)];
-    case Queen: return l->bishopAttacks[s][magicHash(l->bishopMagics[s], occupancies)] |
-                       l->rookAttacks[s][magicHash(l->rookMagics[s], occupancies)];
+    case Bishop: return l->bishopAttacks[s][hash(occupancies, l->bishopBits[s])];
+    case Rook: return l->rookAttacks[s][hash(occupancies, l->rookBits[s])];
+    case Queen: return l->bishopAttacks[s][hash(occupancies, l->bishopBits[s])] |
+                       l->rookAttacks[s][hash(occupancies, l->rookBits[s])];
     default: printf("Piece: %d\n", t); // "Invalid piece type\n
              fprintf(stderr, "Invalid piece type\n"); exit(EXIT_FAILURE);
   }
@@ -131,9 +115,8 @@ static BitBoard getAttacks(Square s, Type t, BitBoard occupancies) {
       else if (t <= Knight && steps > 1) break;
 
       BitBoard attack = getMove(s, t, d, steps);
-      bool capture = attack & occupancies;
       attacks |= attack;
-      if (capture) break;
+      if (attack & occupancies) break;
     }
   }
   return attacks;
@@ -141,13 +124,13 @@ static BitBoard getAttacks(Square s, Type t, BitBoard occupancies) {
 
 // Assume knight or queen will not be passed to this function
 static BitBoard getRelevantBits(Square s, Type t) {
-  BitBoard relevantBits = getAttacks(s, t, EMPTY_BOARD);
+  BitBoard relevantBits = EMPTY_BOARD;
+  relevantBits = getAttacks(s, t, EMPTY_BOARD);
   BitBoard piece = BitBoardSetBit(EMPTY_BOARD, s);
   if (piece & ~NORTH_EDGE) relevantBits &= ~NORTH_EDGE;
   if (piece & ~SOUTH_EDGE) relevantBits &= ~SOUTH_EDGE;
   if (piece & ~EAST_EDGE) relevantBits &= ~EAST_EDGE;
   if (piece & ~WEST_EDGE) relevantBits &= ~WEST_EDGE;
-
   return relevantBits;
 }
 
@@ -184,84 +167,6 @@ static BitBoard getBitsSubset(int index, BitBoard bits) {
     if (index & (1 << i)) relevantBitsSubset = BitBoardSetBit(relevantBitsSubset, s);
   }
   return relevantBitsSubset;
-}
-
-static int magicHash(Magic m, BitBoard occupancies) {
-  return (int)(((m.bits & occupancies) * m.magicNumber) >> (m.bitShift));
-}
-
-static Magic getMagic(Square s, Type t, FILE *fp) {
-  Magic m;
-  m.bits = getRelevantBits(s, t);
-  m.bitShift = BOARD_SIZE - BitBoardCountBits(m.bits);
-  // Attempt to read magic number from file
-  int result = fscanf(fp, "%lu", &m.magicNumber);
-  if (result == 1) return m;
-
-  // Generate magic number if reading failed
-  int powersetSize = POWERSET_SIZE((BOARD_SIZE - m.bitShift));
-  BitBoard relevantBitsPowerset[powersetSize], attacks[powersetSize], usedAttacks[powersetSize];
-
-  for (int i = 0; i < powersetSize; i++) {
-    relevantBitsPowerset[i] = getBitsSubset(i, m.bits);
-    attacks[i] = getAttacks(s, t, relevantBitsPowerset[i]);
-  }
-
-  while (true) {
-    uint64_t magicNumberCandidate = getRandomU64() & getRandomU64() & getRandomU64();
-
-    for (int j = 0; j < powersetSize; j++) usedAttacks[j] = EMPTY_BOARD;
-    int collision = false;
-
-    // Test magic index
-    for (int j = 0; j < powersetSize; j++) {
-      m.magicNumber = magicNumberCandidate;
-      int index = magicHash(m, relevantBitsPowerset[j]);
-      if (usedAttacks[index] == EMPTY_BOARD) {
-        usedAttacks[index] = attacks[j];
-      } else if (usedAttacks[index] != attacks[j]) {
-        collision = true;
-        break;
-      }
-    }
-    if (!collision) {
-      fprintf(fp, "%lu\n", m.magicNumber); // Write the new magic number to file
-      break;
-    }
-  }
-
-  return m;
-}
-
-// 64-bit PRNG
-static uint64_t getRandomU64() {
-  uint64_t u1, u2, u3, u4;
-
-  u1 = (uint64_t)(xorshift()) & 0xFFFF;
-  u2 = (uint64_t)(xorshift()) & 0xFFFF;
-  u3 = (uint64_t)(xorshift()) & 0xFFFF;
-  u4 = (uint64_t)(xorshift()) & 0xFFFF;
-
-  return u1 | (u2 << 16) | (u3 << 32) | (u4 << 48);
-}
-
-static uint32_t xorshift() {
-  static uint32_t state = 0;
-  static bool seeded = false;
-
-  // Seed only once
-  if (!seeded) {
-    state = (uint32_t)time(NULL);
-    seeded = true;
-  }
-
-  uint32_t x = state;
-  x ^= x << 13;
-  x ^= x >> 17;
-  x ^= x << 5;
-
-  state = x;
-  return x;
 }
 
 static BitBoard getSquaresBetween(LookupTable l, Square s1, Square s2) {
