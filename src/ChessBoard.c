c462b783174c0ecedce1012d9aaad604f8295d02
Alex Jasson <z5390820@ad.unsw.edu.au>
2024-05-13 13:09:47 +1000

    touch ups

--- src/ChessBoard.c
+++ src/ChessBoard.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <assert.h>
 
 #include "BitBoard.h"
 #include "LookupTable.h"
--- src/perft.c
+++ src/perft.c
@@ -3,7 +3,6 @@
 #include "ChessBoard.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char **argv) {
     // Check arguments
